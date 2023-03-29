e5c245f1e63e9e4d3f6caf28bc39922aba9cdd34
MoveZZZZ <maks.filatov.9494@gmail.com>
2023-03-29 12:19:36 +0200

    Add models. add repositoryFile, little changed Interface, Base Equation v1.0

--- PZ_APP/ViewModels/BaseEquationViewModel.cs
+++ PZ_APP/ViewModels/BaseEquationViewModel.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using PZ_APP.Models;
+using PZ_APP.Repositories;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -31,24 +33,117 @@ namespace PZ_APP.ViewModels
 
         private string _errorMessage;
 
-        private float _baseScore;
-        private float _impact;
-        private float _exploitability;
-        private float _fimpact;
+        private string _baseScoreString;
+        private string _impactString;
+        private string _exploitabilityString;
+        private string _fimpactString;
 
 
-        public string AccessVectorValue { get; }
-        public string AccessComplexityValue { get; }
-        public string AuthenticationValue { get; }
-        public string ConfImpactValue { get; }
-        public string IntegImpact { get; }
-        public string AvailImpact { get; }
+        public string AccessVectorValue 
+        {
+            get { return _selectedItemAccessVectorString; }
+            set
+            {
+                _selectedItemAccessVectorString = value;
+                OnPropertyChanged(nameof(AccessVectorValue));
+            }
+        }
+        public string AccessComplexityValue 
+        { 
+            get { return _selectedItemAccessComplexityString; }
+            set
+            {
+                _selectedItemAccessComplexityString = value;
+                OnPropertyChanged(nameof(AccessComplexityValue));
+            }
+        }
+        public string AuthenticationValue 
+        { 
+            get { return _selectedItemAuthenticationString; }
+            set
+            {
+                _selectedItemAuthenticationString = value;
+                OnPropertyChanged(nameof(AuthenticationValue));
+                
+                
+            }
+        }
+        public string ConfImpactValue 
+        { 
+            get { return _selectedItemConfImpactString; }
+            set
+            {
+               _selectedItemConfImpactString = value;
+               OnPropertyChanged(nameof(ConfImpactValue));
+            }
+        }
+        public string IntegImpactValue 
+        { 
+            get { return _selectedItemIntegImpactString; }
+            set
+            {
 
-
-        public string BaseScore;
-        public string Impact;
-        public string Exploitability;
-        public string FImpakt;
+                _selectedItemIntegImpactString = value;
+                OnPropertyChanged(nameof(IntegImpactValue));
+            }
+        }
+        public string AvailImpactValue 
+        { 
+            get { return _selectedItemAvailImpactString; }
+            set
+            {
+                _selectedItemAvailImpactString = value;
+                OnPropertyChanged(nameof(AvailImpactValue));
+            }
+        }
+        public string BaseScore
+        {
+            get { return _baseScoreString; }
+            set
+            {
+                if(_baseScoreString != value)
+                {
+                    _baseScoreString = value;
+                    OnPropertyChanged(nameof(BaseScore));
+                }
+            }
+        }
+        public string Impact
+        {
+            get { return _impactString; }
+            set
+            {
+                if(_impactString != value)
+                {
+                    _impactString = value;
+                    OnPropertyChanged(nameof(Impact));
+                }
+            }
+        }
+        public string Exploitability
+        {
+            get { return _exploitabilityString; }
+            set
+            {
+                if(_exploitabilityString != value)
+                {
+                    _exploitabilityString = value;
+                    OnPropertyChanged(nameof(Exploitability));
+                }
+            }
+        }
+        public string FImpakt
+        {
+            get { return _fimpactString; }
+            set
+            {
+                if(_fimpactString != value)
+                {
+                    _fimpactString = value;
+                    OnPropertyChanged(nameof(FImpakt));
+                }
+            }
+        }
 
 
         public ComboBoxItem SelectedItemAccessVector
@@ -141,10 +236,15 @@ namespace PZ_APP.ViewModels
 
 
         public ICommand CalculateCommand { get; }
+        private IBaseEquationRepository BaseEquationRepository;
+        private BaseEquationModel _BaseEquationModel;
+
 
         public BaseEquationViewModel()
         {
             ErrorMessage = "Brak danych!";
+            BaseEquationRepository = new BaseEquationRepository();
+            _BaseEquationModel = new BaseEquationModel();
             CalculateCommand = new ViewModelCommand(ExecuteCalculateCommand, CanExecuteCalculateCommand);
         }
 
@@ -153,6 +253,7 @@ namespace PZ_APP.ViewModels
             if (_selectedItemAccessComplexityString != null && _selectedItemAccessVectorString != null && _selectedItemAuthenticationString != null
                 && _selectedItemAvailImpactString != null && _selectedItemConfImpactString != null && _selectedItemIntegImpactString !=null)
             {
+                SetValuesBaseEquationModel();
                 ErrorMessage = "";
                 return true;
             }
@@ -161,7 +262,33 @@ namespace PZ_APP.ViewModels
 
         private void ExecuteCalculateCommand(object obj)
         {
-            throw new NotImplementedException();
+            BaseEquationRepository.setNumberVariables(_BaseEquationModel);
+            BaseEquationRepository.calculateAllValues(_BaseEquationModel);
+            SetCalculationValue();
+        }
+
+        private void SetCalculationValue()
+        {
+            AccessVectorValue = _selectedItemAccessVectorString; /*+ " (" + Convert.ToString(_BaseEquationModel.AccessVectorNumber) + ")"*/
+            AccessComplexityValue = _selectedItemAccessComplexityString; /*+ " (" + Convert.ToString(_BaseEquationModel.AccessComplexityNumber) + ")"*/
+            AuthenticationValue = _selectedItemAuthenticationString;  /*+" (" + Convert.ToString(_BaseEquationModel.AuthenticationNumber) + ")"*/
+            ConfImpactValue = _selectedItemConfImpactString;  /*+" (" + Convert.ToString(_BaseEquationModel.ConfImpactNumber) + ")"*/
+            IntegImpactValue = _selectedItemIntegImpactString;  /*+" (" + Convert.ToString(_BaseEquationModel.IntegImpactNumber) + ")"*/
+            AvailImpactValue = _selectedItemAvailImpactString; /*+ " (" + Convert.ToString(_BaseEquationModel.AvailImpactNumber) + ")"*/
+
+            BaseScore = Convert.ToString(_BaseEquationModel.BaseScore);
+            Impact = Convert.ToString(_BaseEquationModel.Impact);
+            FImpakt = Convert.ToString(_BaseEquationModel.fImpact);
+            Exploitability = Convert.ToString(_BaseEquationModel.Exploitability);
+        }
+        private void SetValuesBaseEquationModel()
+        {
+            _BaseEquationModel.AccessVectorString = _selectedItemAccessVectorString;
+            _BaseEquationModel.AccessComplexityString = _selectedItemAccessComplexityString;
+            _BaseEquationModel.AuthenticationString = _selectedItemAuthenticationString;
+            _BaseEquationModel.ConfImpactString = _selectedItemConfImpactString;
+            _BaseEquationModel.IntegImpactString = _selectedItemIntegImpactString;
+            _BaseEquationModel.AvailImpactString = _selectedItemAvailImpactString;
         }
     }
 }
